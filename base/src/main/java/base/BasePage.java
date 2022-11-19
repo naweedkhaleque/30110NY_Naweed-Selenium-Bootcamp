ee658bcb022fa65c3c4174ba0281e2727ba6c277
naweedkhaleque <nkhaleque17@gmail.com>
2022-11-19 09:27:39 -0500

    url

--- base/src/main/java/base/BasePage.java
+++ base/src/main/java/base/BasePage.java
@@ -85,7 +85,7 @@ public class BasePage {
 
     @Parameters({"driverConfigEnabled", "browser", "url"})
     @BeforeMethod
-    public void driverSetup(@Optional("true") String driverConfigEnabled, @Optional("chrome") String browser, @Optional("http://espn.com") String url) {
+    public void driverSetup(@Optional("true") String driverConfigEnabled, @Optional("chrome") String browser, @Optional("https://www.apartments.com") String url) {
         if (Boolean.parseBoolean(driverConfigEnabled)) {
             driverInit(browser);
             driver.get(url);
