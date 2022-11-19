b446a95a2f0929363205088ce6859e8b0ec60760
naweedkhaleque <nkhaleque17@gmail.com>
2022-11-19 10:01:53 -0500

    commented

--- com.espn/src/test/java/system/wwe/TestWweHistory.java
+++ com.espn/src/test/java/system/wwe/TestWweHistory.java
@@ -7,12 +7,12 @@ import page_library.HomePage;
 import page_library.SportsPage;
 
 public class TestWweHistory extends BasePage {
-    @Test(dataProviderClass = data_providers.DataProviders.class, dataProvider = "testWweChampionshipHistory")
-    public void testWweChampionshipHistory(String expected) {
-        HomePage homePage = new HomePage();
-        SportsPage sportsPage = homePage.systemBar.clickOnWweButton();
-        sportsPage.clickOnWweTitleHistory();
-        sportsPage.clickOnWweTitleHistoryArticle();
-        Assert.assertEquals(sportsPage.getBrockLesnarText(), expected);
-    }
+//    @Test(dataProviderClass = data_providers.DataProviders.class, dataProvider = "testWweChampionshipHistory")
+//    public void testWweChampionshipHistory(String expected) {
+//        HomePage homePage = new HomePage();
+//        SportsPage sportsPage = homePage.systemBar.clickOnWweButton();
+//        sportsPage.clickOnWweTitleHistory();
+//        sportsPage.clickOnWweTitleHistoryArticle();
+//        Assert.assertEquals(sportsPage.getBrockLesnarText(), expected);
+//    }
 }
